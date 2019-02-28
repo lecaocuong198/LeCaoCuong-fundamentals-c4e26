e2f30cf838746d76c407b9478069549188d54fbe
lecaocuong198 <lecaocuong198@gmail.com>
2019-02-28 15:14:06 +0700

    'session7'

--- session7/homework/back-color-problem/check_color.py
+++ session7/homework/back-color-problem/check_color.py
@@ -1,6 +1,6 @@
 def is_inside(x,y,c):
-    if x > c[0] and y>c[1]:
-        if x<c[2]+c[0] and y<c[3]+c[1]:
+    if x >= c[0] and y>=c[1]:
+        if x<c[2]+c[0] and y<=c[3]+c[1]:
             return True
         else:
             return False         
